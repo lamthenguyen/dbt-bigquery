f5d034ceee9d3e9b8efa627b458d09978fb05d6d
Lam The Nguyen <martmal@gmail.com>
2023-06-29 10:31:42 +0000

    update

--- macros/test_column_value.sql
+++ macros/test_column_value.sql
@@ -1,3 +1,3 @@
-{% macro test_column_value(column) %}
+{% macro test test_column_value(column) %}
     {{ log("PASS: Column value test passed for column '" ~ column.column_name ~ "'") }}
 {% endmacro %}
