604fc7a07b44c2d7b8c837233c6a711b07414669
Lam The Nguyen <martmal@gmail.com>
2023-06-29 08:32:11 +0000

    update

--- macros/test_nullable.sql
+++ macros/test_nullable.sql
@@ -1,11 +1,10 @@
--- macros/test_nullable.sql
-{% macro test_nullable(column, column_type) %}
-    {% if column_type == "STRING" %}
+{% macro test_nullable(column) %}
+    {% if column.type == "STRING" %}
         {% if column.nullable %}
             {{ log("PASS: Column '" ~ column.name ~ "' is nullable") }}
         {% else %} {{ log("FAIL: Column '" ~ column.name ~ "' is not nullable") }}
         {% endif %}
-    {% elif column_type == "INTEGER" %}
+    {% elif column.type == "INTEGER" %}
         {% if column.nullable %}
             {{ log("FAIL: Column '" ~ column.name ~ "' is nullable") }}
         {% else %} {{ log("PASS: Column '" ~ column.name ~ "' is not nullable") }}
@@ -13,3 +12,4 @@
     {% else %} {{ log("SKIP: Column '" ~ column.name ~ "' has unsupported type") }}
     {% endif %}
 {% endmacro %}
+
