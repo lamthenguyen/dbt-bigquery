ccefbd7d021826eab1e3b4c55880c4f99b65804a
Lam The Nguyen <martmal@gmail.com>
2023-06-29 07:46:59 +0000

    update schema

---
+++ models/flights_data_mart.sql
@@ -0,0 +1,40 @@
+{{
+    config(
+        materialized="table",
+        destination_dataset="my_first_bigquery",
+        destination_table="flights_data_mart",
+    )
+}}
+
+with
+    source_data as (
+        select
+            `Date`,
+            `Code`,
+            `Number`,
+            `Origin`,
+            `Destination`,
+            `Departure Time`,
+            `Departure Delay`,
+            `Arrival Time`,
+            `Arrival Delay`,
+            `Flight Time`,
+            `Distance`,
+            `Record ID`
+        from `my-project-95615-388109.my_first_bigquery.flights_sample`
+    )
+
+select
+    `Date`,
+    `Code`,
+    `Number`,
+    `Origin`,
+    `Destination`,
+    `Departure Time`,
+    `Departure Delay`,
+    `Arrival Time`,
+    `Arrival Delay`,
+    `Flight Time`,
+    `Distance`,
+    `Record ID`
+from source_data
