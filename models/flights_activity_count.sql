eff61b894ebfa68d913d42f17aa695e46c94f280
Lam The Nguyen <martmal@gmail.com>
2023-07-09 06:11:52 +0000

    test

--- models/flights_activity_count.sql
+++ models/flights_activity_count.sql
@@ -1,9 +1,8 @@
--- models/flights_activity_count.sql
 {{
     config(
         materialized="table",
         destination_dataset="my_first_bigquery",
-        destination_table="flights_data_mart",
+        destination_table="flights_activity_count",
     )
 }}
 
@@ -11,8 +10,7 @@ with
     source_data as (
         select `Code`, `Number`
         from `my-project-95615-388109.my_first_bigquery.flights_sample`
-    ),
-    activity_count as (select count(*) as count from source_data)
+    )
 
-select `Code`, `Number`, (select count from activity_count) as activity_count
+select `Code`, `Number`
 from source_data
