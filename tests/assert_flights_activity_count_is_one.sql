7029c8f618353073102bde0c3fc627a105a7f9e3
Lam The Nguyen <martmal@gmail.com>
2023-07-09 07:07:43 +0000

    test

--- tests/assert_flights_activity_count_is_one.sql
+++ tests/assert_flights_activity_count_is_one.sql
@@ -1,5 +1,10 @@
+with
+    activity_count as (
+        select count(*) as count from {{ ref("flights_activity_count") }}
+    )
+
 select count
-from activity_count
+from {{ ref("activity_count") }}
 where count = 1
 having
     count != 0
